bff2fb191e7e2579698702088a7b25ff6f97e6f6
Albin Bramstång <albin.bramstang@devies.se>
2023-10-26 17:21:47 +0200

    Use immutable type correctly

--- KarlaTower/ElevatorApi/Repositories/MusicRepository.cs
+++ KarlaTower/ElevatorApi/Repositories/MusicRepository.cs
@@ -2,29 +2,20 @@ using System.Collections.Immutable;
 
 namespace KarlaTower.Repositories;
 
-public static class MusicRepository
+public static class MusicRepository // TODO Build an interface
 {
-    public static ImmutableArray<string> ElevatorSongs => new()
-    {
-        "Easy by The Commodores",
-        "Just the Way You Are by Billy Joel",
-        "Smooth by Santana ft. Rob Thomas",
-        "Rock with You by Michael Jackson",
-        "I Will Always Love You by Whitney Houston",
-        "The Way You Look Tonight by Frank Sinatra",
-        "How Deep Is Your Love by Bee Gees",
-        "I Can't Help Falling in Love by Elvis Presley",
-        "Can't Get Enough of Your Love, Babe by Barry White",
-        "Let's Stay Together by Al Green",
-        "Isn't She Lovely by Stevie Wonder",
-        "I'm Yours by Jason Mraz",
-        "Crazy by Gnarls Barkley",
-        "Kiss by Prince",
-        "Smooth Operator by Sade",
-        "My Girl by The Temptations",
-        "Under Pressure by Queen and David Bowie",
-        "I Want to Hold Your Hand by The Beatles",
-        "All of Me by John Legend",
-        "Stayin' Alive by Bee Gees"
-    };
+    public static ImmutableDictionary<int, string> ElevatorSongs =>
+        ImmutableDictionary.CreateRange(new Dictionary<int, string>
+        {
+            { 1, "Easy by The Commodores" },
+            { 2, "Just the Way You Are by Billy Joel" },
+            { 3, "Smooth by Santana ft. Rob Thomas" },
+            { 4, "Rock with You by Michael Jackson" },
+            { 5, "I Will Always Love You by Whitney Houston" },
+            { 6, "The Way You Look Tonight by Frank Sinatra" },
+            { 7, "How Deep Is Your Love by Bee Gees" },
+            { 8, "I Can't Help Falling in Love by Elvis Presley" },
+            { 9, "Can't Get Enough of Your Love, Babe by Barry White" },
+            { 10, "Let's Stay Together by Al Green" }
+        });
 }
